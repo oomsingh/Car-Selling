c83b959b2a3899e04d3a5e75fe0847a5dbba1a3c
Om Singh <115030660+oomsingh@users.noreply.github.com>
2022-11-10 18:08:55 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-
 //connect to database
 $db=mysqli_connect("localhost","colourc5_bus","Shivsingh1@","colourc5_bus");
-
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,63 +15,7 @@ $db=mysqli_connect("localhost","colourc5_bus","Shivsingh1@","colourc5_bus");
   <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<div class="container">
-  <hgroup>
-    <h1 class="site-title" style="text-align: center; color: green;">Welcome <?php echo $_SESSION['username']; ?> to CarSelling</h1><br>
-
-  </hgroup>
-
-<br>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-  <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav center">
-        <li><a href="logout.php">Logout</a></li
-      </ul>
-
-    </div>
-  </div>
-</nav>
-
-
-<main class="main-content">
- <div class="col-md-6 col-md-offset-4">
-<?php
-    if(isset($_SESSION['message']))
-    {
-         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
-         unset($_SESSION['message']);
-    }
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
+  
 <style>
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
@@ -102,9 +45,39 @@ $db=mysqli_connect("localhost","colourc5_bus","Shivsingh1@","colourc5_bus");
 .card button:hover {
   opacity: 0.7;
 }
-</style>
+</style> 
+  
 </head>
 <body>
+
+<div class="container">
+  <hgroup>
+    <h1 class="site-title" style="text-align: center; color: green;">Welcome <?php echo $_SESSION['username']; ?> to CarSelling</h1><br>
+
+  </hgroup>
+
+<br>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+  <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav center">
+        <li><a href="logout.php">Logout</a></li
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+<main class="main-content">
+ <div class="col-md-6 col-md-offset-4">
+<?php
+    if(isset($_SESSION['message']))
+    {
+         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+         unset($_SESSION['message']);
+    }
+?>
 
 <br><br>
 <div class="card">
@@ -114,10 +87,7 @@ $db=mysqli_connect("localhost","colourc5_bus","Shivsingh1@","colourc5_bus");
   <p>The BMW X5 has 2 Diesel Engine and 2 Petrol Engine on offer.</p>
   <p><button><a href="contact.php">Contact us to Buy</a></button></p>
 </div>
-
 <br><br>
-
-
 </body>
 </html>
 
@@ -131,9 +101,3 @@ $db=mysqli_connect("localhost","colourc5_bus","Shivsingh1@","colourc5_bus");
 
 
 
-
-
-
-
-</body>
-</html>
